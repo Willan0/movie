5efd6f997b3e037ef7b6651118188d8981a054ac
haruto <uchikaharu0125@gmail.com>
2023-01-26 12:15:32 +0630

    movie

--- lib/widgets/text.dart
+++ lib/widgets/text.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../constant/colors.dart';
 import '../constant/dimens.dart';
@@ -26,7 +27,13 @@ class TextWidgets extends StatelessWidget {
           const Text(voting,style: TextStyle(color: tColor),),
           const Gap(kG),
           IconButton(
-              onPressed: (){},
+              onPressed: () async{
+                if(await canLaunchUrl(Uri.parse('https://www.youtube.com/watch?v=MmoBvmJA9XI'))){
+                  launchUrl(Uri.parse('https://www.youtube.com/watch?v=MmoBvmJA9XI'),mode: LaunchMode.externalApplication);
+                }else{
+                  print("Url launch error");
+                }
+              },
               icon: const Icon(Icons.play_circle_outline_outlined,size: kIs,color: bColor))
         ],
       ),
